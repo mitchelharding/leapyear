2abf8824920c9569deb3a68ff781c4f53b23c2ff
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-16 09:09:33 -0700

    add bussiness logic returning false if number(year) is divisable by 100

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var leapYear = function(year){
-  if (year % 4 === 0){
-  return true;
-} else {
-  return false;
+  if (year % 100 === 0 ) {
+    return false;
+  } else if (year % 4 === 0){
+    return true;
+  } else {
+    return false;
   }
 };
 
