d44fbe6c71b528b9ed4447bbb4337b4f2c080770
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-16 09:05:17 -0700

    added specs which, upon submission of a number(year), will check to see if this year is successfully divide by 4 and return either true or false

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var leapYear = function(year){
+  if (year % 4 === 0){
+  return true;
+} else {
+  return false;
+  }
+};
+
+//jQuery user interface logic here.
+$(document).ready(function(){
+  $("form#leap-year").submit(function(){
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
